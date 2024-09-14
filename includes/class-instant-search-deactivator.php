45f463be77802ba1d21868d388f0c5f4c0a1cb2b
Vecsei Szilveszter <weszty@yahoo.com>
2024-09-14 22:24:26 +0300

    activation / deactivation of plugin
    small details: translation support and no directory browsing allowed

---
+++ includes/class-instant-search-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://https://www.marincas.net
+ * @since      1.0.0
+ *
+ * @package    Instant_Search
+ * @subpackage Instant_Search/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Instant_Search
+ * @subpackage Instant_Search/includes
+ * @author     webmaster85 <weszty@wesztyweb.com>
+ */
+class Instant_Search_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
--- instant-search.php
+++ instant-search.php
@@ -1,20 +1,62 @@
-<?php 
-    if ( ! defined( 'ABSPATH' ) ) {
-        exit; // Exit if accessed directly
-    }
-?>
 <?php
+
 /**
- * Plugin Name: Instant Search
- * Plugin URI: https://instant-search.net
- * Description: A WordPress search plugin with live and voice search.
- * Version: 1.0.9
- * Author: webmaster85
- * Author URI: https://www.marincas.net
- * License: GPLv2 or later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * The plugin core
+ *
+ * This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://https://www.marincas.net
+ * @since             1.0.0
+ * @package           Instant_Search
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Instant Search
+ * Plugin URI:        https://https://instant-search.net
+ * Description:       A WordPress search plugin with live and voice search.
+ * Version:           1.0.0
+ * Author:            webmaster85
+ * Author URI:        https://https://www.marincas.net/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       instant-search
+ * Domain Path:       /languages
  */
- 
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'INSTANT_SEARCH_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-instant-search-activator.php
+ */
+function activate_instant_search() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-instant-search-activator.php';
+	Instant_Search_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-instant-search-deactivator.php
+ */
+function deactivate_instant_search() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-instant-search-deactivator.php';
+	Instant_Search_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_instant_search' );
+register_deactivation_hook( __FILE__, 'deactivate_instant_search' );
+
 function instant_search_form() {
     ob_start(); 
     include plugin_dir_path(__FILE__) . 'searchform.php'; 
@@ -142,31 +184,6 @@ function flush_search_queries() {
     wp_redirect(admin_url('admin.php?page=instant_search'));
     exit;
 }
-
-function instant_search_activate() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'instant_search_queries'; 
-    $charset_collate = $wpdb->get_charset_collate();
-    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        query varchar(255) NOT NULL,
-        count mediumint(9) NOT NULL DEFAULT 1,
-        PRIMARY KEY  (id)
-    ) $charset_collate;";
-
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql); 
-}
-register_activation_hook(__FILE__, 'instant_search_activate');
-
-function instant_search_check_table() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'instant_search_queries';
-    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-        instant_search_activate();
-    }
-}
-add_action('plugins_loaded', 'instant_search_check_table');
 
 function log_search_query($query) {
     global $wpdb;
