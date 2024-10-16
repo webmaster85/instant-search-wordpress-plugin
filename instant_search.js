349e8f24a2e1237cb69437a8269c3fc5c0088b18
Marius <contact@marincas.net>
2024-10-16 14:21:05 +0300

    Update instant_search.js

--- instant_search.js
+++ instant_search.js
@@ -1,9 +1,9 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function($) {	
     var searchMethod = instant_search.search_method;
-    var displayStyle = instant_search.display_style;
+    var displayStyle = instant_search.display_style;	
     function getOrCreateOverlay() {
         let overlay = $('#search-overlay');
-        if (overlay.length === 0) {
+        if (overlay.length === 0) {		
             overlay = $('<div id="search-overlay" class="search-overlay" style="display: none;"></div>');
             let searchResults = $('<div id="search-results" class="' + displayStyle + '" style="display: none;"></div>');
             overlay.append(searchResults);
@@ -11,20 +11,21 @@ jQuery(document).ready(function($) {
         }
         return overlay;
     }
-    var currentRequest = null;
+    var currentRequest = null;	
     function getOrCreateInlineWrapper() {
         let wrapper = $('#inline-search-results');
-        if (wrapper.length === 0) {
+        if (wrapper.length === 0) {	
             wrapper = $('<div id="inline-search-results" class="search-results ' + displayStyle + '"></div>');
             $('#s2').closest('.search-wrapper2').append(wrapper);
         }
         return wrapper;
-    }
+    }	
     function performSearch(query) {       
         var overlay = getOrCreateOverlay();
         var searchResults = overlay.find('#search-results');
         var inlineWrapper = getOrCreateInlineWrapper();
-        if (query === '') {	
+
+        if (query === '') {		
             if (searchMethod === 'overlay') {
                 overlay.fadeOut(0, function() {
                     searchResults.hide();
@@ -37,7 +38,7 @@ jQuery(document).ready(function($) {
                 currentRequest.abort();
             }
             currentRequest = $.ajax({
-                url: instant_search.ajax_url,
+                url: instant_search.endpoint_url, 
                 type: 'post',
                 data: {
                     action: 'instant_search',
@@ -56,20 +57,23 @@ jQuery(document).ready(function($) {
                 }
             });
         }
-    }
-    $('#s, #s2').on('input', function() {
-        var query = $('#s').val() || $('#s2').val();
-        performSearch(query);
-    });
-    if (searchMethod === 'overlay') {
-        $(document).on('click', '#search-overlay', function(e) {
-            if ($(e.target).closest('#search-results').length === 0) {
-                $('#search-overlay').fadeOut(function() {
-                    $(this).remove();
-                });
+    }	
+	let debounceTimer;
+		$('#s, #s2').on('input', function() {
+			clearTimeout(debounceTimer);
+			debounceTimer = setTimeout(() => {
+				var query = $('#s').val() || $('#s2').val();
+				performSearch(query);
+			}, 300); // Debounce by 300ms
+		});
+   if (searchMethod === 'overlay' || searchMethod === 'inline') {
+        $(document).on('click', function(e) {
+            if (!$(e.target).closest('#search-results').length && !$(e.target).closest('#s, #s2').length) {
+                $('#search-overlay, #inline-search-results').fadeOut(); 
+                $('#s, #s2').val(''); 
             }
         });
-    }
+    }	
     $(document).on('click', '#search-results', function(e) {
         e.stopPropagation();
     });	
@@ -83,11 +87,10 @@ jQuery(document).ready(function($) {
             var cleanedTranscript = transcript.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
             $('#s, #s2').val(cleanedTranscript).trigger('input'); 
         };
-
         $('#voice-search-btn, #voice-search-btn2').on('click', function() {
             recognition.start(); 
         });
     } else {
         $('#voice-search-btn, #voice-search-btn2').hide(); 
-    }
+    }	
 });
